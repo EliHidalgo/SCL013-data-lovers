85708038cf2ab12339427269c0e006c32469161c
EliHidalgo <hidalgo_eliana@hotmail.com>
2020-05-03 14:01:10 -0400

    Funcionalidad filtro por Tipo y muestra la card chica con el pokemon

--- src/data.js
+++ src/data.js
@@ -3,19 +3,70 @@
 export const example = () => {
   return 'example';
 };
-
 export const anotherExample = () => {
   return 'OMG';
 };*/
-export const filterData = (pokemonDatos, condition) => {
+/*export const filterData = (pokemonDatos, condition) => {
   //console.log(pokemonDatos);
   //console.log(condition);
-  
-  let names = [];
-  let idPokem
-  for (let i=0 ; i< pokemonDatos.length; i++){
-    names.push(pokemonDatos[i].name)
+  let types = [];
+  for(let i = 0; i < pokemonDatos.length; i++) {
+    types.push(pokemonDatos[i].type)
   }
-  return names;
+  let type = pokemonDatos.filter((pokemonDatos) => pokemonDatos.type === condition);
+  return type;
+  //for (let i=0 ; i< pokemonDatos.length; i++){
+   // names.push(pokemonDatos[i].name)
+ // }
+  //return names;
   //console.log(names);
+}*/
+
+import pokemonData from './../data/pokemon/pokemon.js';
+
+const pokemonDatos = pokemonData.pokemon;
+console.log(pokemonDatos);
+
+const imgOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.img);
 }
+
+const nameOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.name);
+}
+
+const numOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.num);
+}
+
+const typeOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.type);
+}
+
+const heightOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.height);
+}
+
+const weightOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.weight);
+}
+
+const candyOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.candy);
+}
+
+const candy_countOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.candy_count).map((x) => x !== undefined ? x : "No posee siguiente evolución");
+}
+
+const weaknessesOfPokemon = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.weaknesses);
+}
+
+const next_evolution = (newFilter, condition) => {
+  return pokemonDatos.filter((pokemon) => pokemon[newFilter].includes(condition)).map((pokemon) => pokemon.next_evolution.name).map((x) => x !== undefined ? x : "No tiene mas evoluciones");
+}
+
+
+export { imgOfPokemon, nameOfPokemon, numOfPokemon, typeOfPokemon, heightOfPokemon, weightOfPokemon, candyOfPokemon, candy_countOfPokemon,
+        weaknessesOfPokemon, next_evolution };
--- src/main.js
+++ src/main.js
@@ -1,39 +1,78 @@
-//import { example } from './data.js';
-import pokemonData from './../data/pokemon/pokemon.js';
-import { filterData } from './data.js';
+//import pokemonData from './../data/pokemon/pokemon.js';
+import { imgOfPokemon, nameOfPokemon, numOfPokemon, typeOfPokemon, heightOfPokemon, weightOfPokemon,
+        candyOfPokemon, candy_countOfPokemon, weaknessesOfPokemon, next_evolution } from './data.js';
 
 // constante para traer los valores de la propiedad (key) pokemon del objeto pokemonData
-const pokemonDatos = pokemonData.pokemon;
+//const pokemonDatos = pokemonData.pokemon;
 // mostrando pokemonDatos que es un arreglo de objetos
-console.log(pokemonDatos);
+//console.log(pokemonDatos);
+
+
 // mostrando el primer valor del arreglo pokemonDatos (indexOf 0)
-console.log(pokemonDatos[0]);
+/*console.log(pokemonDatos[0]);
 // mostrando la primera propiedad que conforma el objeto ubicado en el index 0 del arreglo 
 console.log(pokemonDatos[0].name);
 console.log(pokemonDatos[0].id);
 console.log(pokemonDatos[0].type);
 console.log(pokemonDatos[0].candy);
 console.log(pokemonDatos[0].candy_count);
-console.log(pokemonDatos[0].weaknesses);
+console.log(pokemonDatos[0].weaknesses);*/
 
-
-const selecTypePokemon = document.getElementById("selectType");
-const cardsContainer = document.getElementById("root");
+//variable para el id del selectType, seleeción por tipo
+const selecFilterByTypePokemon = document.getElementById("selectType");
+//variable para el id del container de la pagina principal de bienvenida
 const welcomeContainer = document.getElementById("welcomePage");
-console.log(selecTypePokemon);
 
-//selectTypePokemon.options[selectTypePokemon.selectedIndex].text ese codigo reconoce cual de todos los options fue seleccionado y te devulve el valor de ese option
-
-selecTypePokemon.addEventListener('change', () => {
-    let condition = selecTypePokemon.options[selecTypePokemon.selectedIndex].text;
-    //console.log(condition);
-    let namesResult = filterData(pokemonDatos, condition);
+//evento de select para filtrar por tipo de pokémon
+selecFilterByTypePokemon.addEventListener('change', () => {
+    //variable que reconoce cual de todos los options fue seleccionado en el droplist y te devulve el valor de ese option
+    const droplistSelectType = selecFilterByTypePokemon.options[selecFilterByTypePokemon.selectedIndex].value;
+    //creando las variables de string de la información que debe aparecer en las cards de cada pokémon
+    const stringOfImgOfPokemon = imgOfPokemon("type", droplistSelectType);
+    const stringOfNameOfPokemon = nameOfPokemon("type", droplistSelectType);
+    const stringOfNumOfPokemon = numOfPokemon("type", droplistSelectType);
+    const stringOfTypeOfPokemon = typeOfPokemon("type", droplistSelectType);
     
-    for(let i = 0; i < namesResult.length; i++){
-        cardsContainer.innerHTML += `<p> ${namesResult[i]}</p>`;
-       // cardsContainer.innerHTML += `<img src ="${pokemonDatos} `
-        welcomeContainer.innerHTML = " ";
-
+    //variable que llama la sección (root) que contendra todas las cards de los pokémon
+    const cardsContainer = document.getElementById("root");
+    //ciclo para borrar todos los hijos que pueda tener la seccion de root (cardsContainer) para poder imprimir los pokémon por filtro solicitado
+    while (cardsContainer.hasChildNodes()) {
+        cardsContainer.removeChild(cardsContainer.firstChild);
     }
+    //devuleve los otros select de filtro debilidad y ordenar por  para que queden en su select inicial
+    document.getElementById("selectWeak").value = ""; 
+    document.getElementById("selectOrder").value = "";
+    document.getElementById("searchBar").value = ""; // vacia el input dado el caso tenga alguna palabra escrita
     
-})
+    let showingTypeOfPokemon = document.getElementById("topMessageOfType");
+    /*showingTypeOfPokemon.classList.add("showingTypeStyle");
+    cardsContainer.appendChild(showingTypeOfPokemon);*/
+    showingTypeOfPokemon.innerHTML = `<h2 class="showingType"> Tipo de Pokémon: ${droplistSelectType}</h2>`
+    
+    //ciclo para imprimir las cards pequeñas de pokémon por separado, cuando se selecciona el filtro de tipo
+    for(let i = 0; i < stringOfTypeOfPokemon.length; i++){
+        const smallCard = document.createElement("div"); //variable para crear div para las cards de pokémon
+        /*const containerForSmallCards = document.createElement("div"); //variasble que contenga el div de smallCard
+        containerForSmallCards.setAttribute("id", "container-for-small-cards");*/
+        smallCard.classList.add("smallCardStyle"); //agrega atributo de clase
+        smallCard.setAttribute("id", "divSmallCardPokemon");
+        smallCard.innerHTML += `<img id="imgPokemonCard" src= ${stringOfImgOfPokemon[i]}>
+                                <h4 id="pokemonNameCard">${stringOfNameOfPokemon[i]} # ${stringOfNumOfPokemon[i]}</h4>
+                                <p><span class="modalPokemon">Tipo: </span>${stringOfTypeOfPokemon[i]}</p>
+                                <p><span class="seeMorePopUp">Ver...</span></p>`;
+        cardsContainer.appendChild(smallCard);
+        /*containerForSmallCards.appendChild(smallCard);*/
+        welcomeContainer.innerHTML = " "; //borrar la sección de bienvenida para mostrar la seccion de root (cards container)
+
+        //creacion del div que contendra el modal
+
+    
+    
+    
+    
+    
+    }
+
+
+    
+});
